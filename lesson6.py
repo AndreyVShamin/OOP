9908472c795c768dc1c36de73509a82e8bc64566
AndreyVShamin <addsharming@mail.ru>
2021-07-11 13:40:22 +0300

    Testing "test:bool"

--- lesson6.py
+++ lesson6.py
@@ -1,8 +1,19 @@
 # Методы инициализации
 
 class Cat:
+    """
+    OOP6
+    """
 
     def __init__(self, name, breed='pers', age=1, color='white', test: bool = True):
+        """
+        oop6 __init__
+        :param name: string
+        :param breed: string
+        :param age: int
+        :param color: string
+        :param test: bool
+        """
         print('hello new object is ', self, name, breed, age, color)
         self.name = name
         self.age = age
